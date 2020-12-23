cdca4d0971870240a70b53193463d35d10f4004e
Rizki Sunaryo <rizkisunaryo@gmail.com>
2020-12-23 11:48:49 +0700

    Separate AyahRangePicker and separate it

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import {SafeAreaView, View, StatusBar, Platform, TextInput} from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StatusBar,
+  Platform,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import RangeSlider from 'rn-range-slider';
 
 import CustomText from './components/atoms/CustomText';
-import {COLOR_PRIMARY, COLOR_BLUE_2, COLOR_GREY} from './constants/Colors';
+import {COLOR_PRIMARY, COLOR_BLUE_2} from './constants/Colors';
 import CustomTouchableOpacity from './components/atoms/CustomTouchableOpacity';
 import {STYLE_BOX_SHADOW} from './constants/Styles';
+import AyahRangePicker from './components/organisms/AyahRangePicker';
 
 function Burger() {
   return (
@@ -32,146 +39,61 @@ function Burger() {
 
 function App() {
   return (
-    <SafeAreaView style={{paddingTop: 8, height: '100%'}}>
-      <StatusBar
-        translucent
-        backgroundColor="transparent"
-        barStyle="dark-content"
-      />
-      <View style={{height: Platform.select({android: 24})}} />
-      <View
-        style={{
-          marginLeft: 22,
-          marginBottom: 6,
-          marginRight: 32,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
-        <CustomTouchableOpacity>
-          <Burger />
-        </CustomTouchableOpacity>
-        <CustomText style={{color: COLOR_PRIMARY}}>
-          Yuk menghafal Al-Quran!
-        </CustomText>
-      </View>
-
-      <View style={[STYLE_BOX_SHADOW, {paddingHorizontal: 32}]}>
-        <LinearGradient
-          locations={[0, 1]}
-          colors={[COLOR_BLUE_2, COLOR_PRIMARY]}
-          style={[
-            {
-              height: 94,
-              borderRadius: 16,
-              paddingLeft: 35,
-              justifyContent: 'center',
-            },
-            STYLE_BOX_SHADOW,
-          ]}>
-          <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
-            <CustomText bold style={{fontSize: 24, color: 'white'}}>
-              Al-Baqarah{' '}
-            </CustomText>
-            <CustomText style={{color: 'white'}}>(286 Ayat)</CustomText>
-          </View>
-        </LinearGradient>
-
-        <View style={{height: 16}} />
-
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={{paddingTop: 8, height: '100%'}}>
+        <StatusBar
+          translucent
+          backgroundColor="transparent"
+          barStyle="dark-content"
+        />
+        <View style={{height: Platform.select({android: 24})}} />
         <View
-          style={[
-            STYLE_BOX_SHADOW,
-            {
-              padding: 32,
-              backgroundColor: 'white',
-              borderRadius: 16,
-            },
-          ]}>
-          <View style={{flexDirection: 'row'}}>
-            <CustomText style={{color: COLOR_PRIMARY}}>Surat Ayat</CustomText>
-            <View style={{width: 9}} />
-            <CustomText style={{color: COLOR_GREY}}>(99 - 282)</CustomText>
-          </View>
+          style={{
+            marginLeft: 22,
+            marginBottom: 6,
+            marginRight: 32,
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <CustomTouchableOpacity>
+            <Burger />
+          </CustomTouchableOpacity>
+          <CustomText style={{color: COLOR_PRIMARY}}>
+            Yuk menghafal Al-Quran!
+          </CustomText>
+        </View>
 
-          <View style={{height: 16}} />
-
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}>
-            <TextInput
-              style={{
-                flex: 1,
-                height: 24,
-                borderColor: COLOR_PRIMARY,
-                borderWidth: 1,
-                borderStyle: 'solid',
-                borderRadius: 50,
-                color: COLOR_PRIMARY,
-                paddingVertical: 0,
-                paddingHorizontal: 8,
-                textAlign: 'right',
-              }}
-            />
-            <View style={{width: 47, alignItems: 'center'}}>
-              <CustomText bold style={{color: COLOR_PRIMARY}}>
-                -
+        <View style={[STYLE_BOX_SHADOW, {paddingHorizontal: 32}]}>
+          <LinearGradient
+            locations={[0, 1]}
+            colors={[COLOR_BLUE_2, COLOR_PRIMARY]}
+            style={[
+              {
+                height: 94,
+                borderRadius: 16,
+                paddingLeft: 35,
+                justifyContent: 'center',
+              },
+              STYLE_BOX_SHADOW,
+            ]}>
+            <View style={{flexDirection: 'row', alignItems: 'flex-end'}}>
+              <CustomText bold style={{fontSize: 24, color: 'white'}}>
+                Al-Baqarah{' '}
               </CustomText>
+              <CustomText style={{color: 'white'}}>(286 Ayat)</CustomText>
             </View>
-            <TextInput
-              style={{
-                flex: 1,
-                height: 24,
-                borderColor: COLOR_PRIMARY,
-                borderWidth: 1,
-                borderStyle: 'solid',
-                borderRadius: 50,
-                color: COLOR_PRIMARY,
-                paddingVertical: 0,
-                paddingHorizontal: 8,
-                textAlign: 'right',
-              }}
-            />
-          </View>
+          </LinearGradient>
 
           <View style={{height: 16}} />
 
-          <RangeSlider
-            min={1}
-            max={286}
-            step={1}
-            floatingLabel
-            renderThumb={() => (
-              <View
-                style={{
-                  width: 16,
-                  height: 16,
-                  borderColor: COLOR_PRIMARY,
-                  borderWidth: 1,
-                  borderRadius: 16,
-                }}
-              />
-            )}
-            renderRail={() => (
-              <View
-                style={{
-                  width: '100%',
-                  height: 2,
-                  backgroundColor: COLOR_GREY,
-                }}
-              />
-            )}
-            renderRailSelected={() => (
-              <View style={{height: 2, backgroundColor: COLOR_PRIMARY}} />
-            )}
-            onValueChanged={(low: any, high: any) => console.log(low, high)}
+          <AyahRangePicker
+            numberOfAyah={286}
+            onRangeChange={(min, max) => console.log(min, max)}
           />
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 }
 
---
+++ src/components/organisms/AyahRangePicker.tsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import {TextInput, View} from 'react-native';
+import RangeSlider from 'rn-range-slider';
+
+import {COLOR_GREY, COLOR_PRIMARY} from '../../constants/Colors';
+import {STYLE_BOX_SHADOW} from '../../constants/Styles';
+import CustomText from '../atoms/CustomText';
+
+function AyahRangePicker(props: {
+  numberOfAyah: number;
+  onRangeChange: (minAyah: number, maxAyah: number) => void;
+}) {
+  const [minAyahText, setMinAyahText] = React.useState(1 + '');
+  const [minAyah, setMinAyah] = React.useState(1);
+
+  const [maxAyah, setMaxAyah] = React.useState(props.numberOfAyah);
+  const [maxAyahText, setMaxAyahText] = React.useState(props.numberOfAyah + '');
+
+  return (
+    <View
+      style={[
+        STYLE_BOX_SHADOW,
+        {
+          padding: 32,
+          backgroundColor: 'white',
+          borderRadius: 16,
+        },
+      ]}>
+      <View style={{flexDirection: 'row'}}>
+        <CustomText style={{color: COLOR_PRIMARY}}>Ayat</CustomText>
+        <View style={{width: 9}} />
+        <CustomText style={{color: COLOR_GREY}}>
+          ({minAyah} - {maxAyah})
+        </CustomText>
+      </View>
+
+      <View style={{height: 16}} />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}>
+        <TextInput
+          value={minAyahText}
+          keyboardType="numeric"
+          onChangeText={(text) => {
+            setMinAyahText(text);
+          }}
+          onBlur={() => {
+            let theNumber = Number(minAyahText.split(/\D/).join(''));
+            if (!theNumber || theNumber < 1) {
+              theNumber = 1;
+            } else if (theNumber > maxAyah) {
+              theNumber = maxAyah;
+            }
+            setMinAyah(theNumber);
+            setMinAyahText(theNumber + '');
+          }}
+          style={{
+            flex: 1,
+            height: 24,
+            borderColor: COLOR_PRIMARY,
+            borderWidth: 1,
+            borderStyle: 'solid',
+            borderRadius: 50,
+            color: COLOR_PRIMARY,
+            paddingVertical: 0,
+            paddingHorizontal: 8,
+            textAlign: 'right',
+          }}
+        />
+        <View style={{width: 47, alignItems: 'center'}}>
+          <CustomText bold style={{color: COLOR_PRIMARY}}>
+            -
+          </CustomText>
+        </View>
+        <TextInput
+          value={maxAyahText}
+          keyboardType="numeric"
+          onChangeText={(text) => {
+            setMaxAyahText(text);
+          }}
+          onBlur={() => {
+            let theNumber = Number(maxAyahText.split(/\D/).join(''));
+            if (!theNumber || theNumber > props.numberOfAyah) {
+              theNumber = props.numberOfAyah;
+            } else if (theNumber < minAyah) {
+              theNumber = minAyah;
+            }
+            setMaxAyah(theNumber);
+            setMaxAyahText(theNumber + '');
+          }}
+          style={{
+            flex: 1,
+            height: 24,
+            borderColor: COLOR_PRIMARY,
+            borderWidth: 1,
+            borderStyle: 'solid',
+            borderRadius: 50,
+            color: COLOR_PRIMARY,
+            paddingVertical: 0,
+            paddingHorizontal: 8,
+            textAlign: 'right',
+          }}
+        />
+      </View>
+
+      <View style={{height: 16}} />
+
+      <RangeSlider
+        min={1}
+        max={props.numberOfAyah}
+        low={minAyah}
+        high={maxAyah}
+        step={1}
+        floatingLabel
+        renderThumb={() => (
+          <View
+            style={{
+              width: 16,
+              height: 16,
+              borderColor: COLOR_PRIMARY,
+              borderWidth: 1,
+              borderRadius: 16,
+            }}
+          />
+        )}
+        renderRail={() => (
+          <View
+            style={{
+              width: '100%',
+              height: 2,
+              backgroundColor: COLOR_GREY,
+            }}
+          />
+        )}
+        renderRailSelected={() => (
+          <View style={{height: 2, backgroundColor: COLOR_PRIMARY}} />
+        )}
+        onValueChanged={(min, max) => {
+          if (
+            Number(minAyahText) !== minAyah ||
+            Number(maxAyahText) !== maxAyah
+          ) {
+            return;
+          }
+
+          setMinAyah(min);
+          setMinAyahText(min + '');
+
+          setMaxAyah(max);
+          setMaxAyahText(max + '');
+
+          props.onRangeChange(min, max);
+        }}
+      />
+    </View>
+  );
+}
+
+export default AyahRangePicker;
